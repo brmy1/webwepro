3ccfb56b11746baff6fff741657c6a9bea3e35f8
Matheus Esturiao <matheussturiao@gmail.com>
2021-11-18 15:17:40 -0300

    master

--- pages/users/new.vue
+++ pages/users/new.vue
@@ -5,18 +5,22 @@ b-form
       h4 Adicionar usuario
       small Informação básica do usuário
 
+    b-col
+      thumbnails-default
+      input(type="file" @change="event => upladProfileImage(event)")
+
   b-row.mt-3
     b-col(sm="12" md="6" )
-      b-form-group#fieldset-name(description='Defina o nome do usuário.' label='Nome' label-for='input-name' valid-feedback='Thank you!' :invalid-feedback='invalidFeedback' :state='state')
-        b-form-input#input-name(v-model='name' :state='state' trim)
+      b-form-group#fieldset-name(description='Defina o nome do usuário.' label='Nome' label-for='input-name')
+        b-form-input#input-name(v-model='form.default' trim)
 
     b-col(sm="12" md="6" )
-      b-form-group#fieldset-lastname(description='Defina o sobrenome usuário.' label='Sobrenome' label-for='input-lastname' valid-feedback='Thank you!' :invalid-feedback='invalidFeedback' :state='state')
-        b-form-input#input-lastname(v-model='name' :state='state' trim)
+      b-form-group#fieldset-lastname(description='Defina o sobrenome usuário.' label='Sobrenome' label-for='input-lastname')
+        b-form-input#input-lastname(v-model='form.default' trim)
 
     b-col
-      b-form-group#fieldset-email(description='Email de acesso do usuário.' label='Email' label-for='input-email' valid-feedback='Thank you!' :invalid-feedback='invalidFeedback' :state='state')
-        b-form-input#input-email(v-model='name' :state='state' trim)
+      b-form-group#fieldset-email(description='Email de acesso do usuário.' label='Email' label-for='input-email')
+        b-form-input#input-email(v-model='form.default' trim)
 
   b-row.mt-5
     b-col
@@ -25,29 +29,29 @@ b-form
 
   b-row.mt-3
     b-col(sm="12" md="4" )
-      b-form-group#fieldset-zip(description='Endereço CEP.' label='CEP' label-for='input-zip' valid-feedback='Thank you!' :invalid-feedback='invalidFeedback' :state='state')
-        b-form-input#input-zip(v-model='name' :state='state' trim)
+      b-form-group#fieldset-zip(description='Endereço CEP.' label='CEP' label-for='input-zip')
+        b-form-input#input-zip(v-model='form.default' trim)
 
     b-col(sm="12" md="8" )
-      b-form-group#fieldset-street(description='Nome da rua.' label='Rua' label-for='input-street' valid-feedback='Thank you!' :invalid-feedback='invalidFeedback' :state='state')
-        b-form-input#input-street(v-model='name' :state='state' trim)
+      b-form-group#fieldset-street(description='Nome da rua.' label='Rua' label-for='input-street')
+        b-form-input#input-street(v-model='form.default' trim)
 
     b-col
-      b-form-group#fieldset-city(description='Cidade.' label='Cidade' label-for='input-city' valid-feedback='Thank you!' :invalid-feedback='invalidFeedback' :state='state')
-        b-form-input#input-city(v-model='name' :state='state' trim)
+      b-form-group#fieldset-city(description='Cidade.' label='Cidade' label-for='input-city')
+        b-form-input#input-city(v-model='form.default' trim)
 
     b-col(sm="12" md="6" )
-      b-form-group#fieldset-email(description='Número de endereço' label='Número' label-for='input-email' valid-feedback='Thank you!' :invalid-feedback='invalidFeedback' :state='state')
-        b-form-input#input-email(v-model='name' :state='state' trim)
+      b-form-group#fieldset-email(description='Número de endereço' label='Número' label-for='input-email')
+        b-form-input#input-email(v-model='form.default' trim)
 
   b-row.mt-4
     b-col(sm="12" md="4" )
-      b-form-group#fieldset-zip(description='Número de contato Whatsapp.' label='Whatsapp' label-for='input-zip' valid-feedback='Thank you!' :invalid-feedback='invalidFeedback' :state='state')
-        b-form-input#input-zip(v-model='name' :state='state' trim)
+      b-form-group#fieldset-zip(description='Número de contato Whatsapp.' label='Whatsapp' label-for='input-zip')
+        b-form-input#input-zip(v-model='form.default' trim)
 
     b-col(sm="12" md="8" )
-      b-form-group#fieldset-street(description='Número fixo.' label='Número de residencia ou loja.' label-for='input-street' valid-feedback='Thank you!' :invalid-feedback='invalidFeedback' :state='state')
-        b-form-input#input-street(v-model='name' :state='state' trim)
+      b-form-group#fieldset-street(description='Número fixo.' label='Número de residencia ou loja.' label-for='input-street')
+        b-form-input#input-street(v-model='form.default' trim)
 
   b-row
     b-col
@@ -57,7 +61,18 @@ b-form
 
 <script>
 export default {
-  name: 'NewUserPage'
+  name: 'NewUserPage',
+  data: () => ({
+    form: {
+      default: ''
+    }
+  }),
+  methods: {
+    upladProfileImage (file) {
+      // eslint-disable-next-line no-console
+      console.warn(file.target.files)
+    }
+  }
 }
 </script>
 
